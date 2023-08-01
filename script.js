ed2355eb63893a17235bde0776289dc750275bf5
durga2677 <125868369+durga2677@users.noreply.github.com>
2023-08-01 12:39:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function volume_sphere() {
     //Write your code here
+            var radius = document.getElementById('radius').value;
+            radius = Math.abs(radius);
+            var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+            volume = volume.toFixed(4);
+            document.getElementById('volume').value = volume;
+            return false;
+        }
+ 
+      window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
   
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
